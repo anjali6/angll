871de8d9e69a4760414b49a9cd23f9d426997d94
anjali6 <anjalikrishnadas6@gmail.com>
2016-06-06 11:58:57 +0530

    Create aan.c

---
+++ aan.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+	scanf("%d",&n);
+	if(n>0)
+	{
+		printf(" \n number is positive");
+	}
+	 else if(n<0)
+	{
+		printf("\n number is negative")
+	}
+	else
+	{
+		printf("\n number is zero")
+	}
+	
+	return 0;
+}
